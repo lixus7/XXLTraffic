f27e96f0acafda20fbf43fcffd385c37cec662d5
Du <yind7@outlook.com>
2024-10-01 11:38:14 +1000

    Create run.sh

---
+++ hourday/run.sh
@@ -0,0 +1,116 @@
+
+model_name=DLinear
+
+python -u run.py \
+  --train_seed 2024 \
+  --samle_rate 1 \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../data/ \
+  --data_path pems10_d.csv \
+  --model_id pems10_d_96_96 \
+  --model $model_name \
+  --data custom \
+  --target '' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 107 \
+  --dec_in 107 \
+  --c_out 107 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.0005 \
+  --train_epoch 200 \
+  --patience 5 \
+  --lradj 'type3' >> dlinear_pems10_d_in96_out96_trseed2024.log 2>&1
+
+
+
+python -u run.py \
+  --train_seed 2024 \
+  --samle_rate 1 \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../data/ \
+  --data_path pems10_d.csv \
+  --model_id pems10_d_96_192 \
+  --model $model_name \
+  --data custom \
+  --target '' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 107 \
+  --dec_in 107 \
+  --c_out 107 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.0005 \
+  --train_epoch 200 \
+  --patience 5 \
+  --lradj 'type3' >> dlinear_pems10_d_in96_out192_trseed2024.log 2>&1
+
+python -u run.py \
+  --train_seed 2024 \
+  --samle_rate 1 \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../data/ \
+  --data_path pems10_d.csv \
+  --model_id pems10_d_96_336 \
+  --model $model_name \
+  --data custom \
+  --target '' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 107 \
+  --dec_in 107 \
+  --c_out 107 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.0005 \
+  --train_epoch 200 \
+  --patience 5 \
+  --lradj 'type3' >> dlinear_pems10_d_in96_out336_trseed2024.log 2>&1
+
+python -u run.py \
+  --train_seed 2024 \
+  --samle_rate 1 \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../data/ \
+  --data_path pems10_d.csv \
+  --model_id pems10_d_96_720 \
+  --model $model_name \
+  --data custom \
+  --target '' \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 107 \
+  --dec_in 107 \
+  --c_out 107 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.0005 \
+  --train_epoch 200 \
+  --patience 5 \
+  --lradj 'type3' >> dlinear_pems10_d_in96_out720_trseed2024.log 2>&1
